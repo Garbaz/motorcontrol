d606005dee335ab365eb21a738016a87737bf164
Garbaz <garbaz@t-online.de>
2016-07-04 16:09:29 +0200

    Added key events and propper info

--- src/client_mc.c
+++ src/client_mc.c
@@ -3,27 +3,34 @@ A GUI client program to allow for friendly control of the motorcontrol network s
 Uses SDL2 and SDL2_ttf.
 
 TODO:
-- Propper info text
 - Actual networking (duh...)
 - ...
 */
-
-
-
 
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <garbaz/netlib.h>
 
 #define FONT_PATH "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
 #define FONT_SIZE 24
 
-#define INFO_TEXT "info!\nmore info..."
+#define INFO_TEXT \
+"W / UP    -- Forward\n"\
+"S / DOWN  -- Backward\n"\
+"A / LEFT  -- Left\n"\
+"D / RIGHT -- Right\n"\
+"Q / ESC   -- Quit\n"
 
 #define WIN_WIDTH 640
 #define WIN_HEIGHT 480
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
 
 #define ERR(...) fprintf(stderr,__VA_ARGS__)
 #define SDLERR ERR("SDL Error: %s\n", SDL_GetError())
@@ -34,6 +41,9 @@ SDL_Surface *sfc, *txt;
 SDL_Event e;
 const SDL_Color black = {255, 255, 255};
 TTF_Font *font;
+
+unsigned char key_state[] = {0, 0, 0, 0};
+unsigned char state_changed = 0;
 
 void quit(int c)
 {
@@ -92,7 +102,79 @@ int main()
 			{
 				quit(0);
 			}
+			else if(e.type == SDL_KEYDOWN)
+			{
+				switch(e.key.keysym.sym)
+				{
+					case SDLK_w:
+					case SDLK_UP:
+						printf("UP pressed\n");
+						key_state[UP] = 1;
+						state_changed = 1;
+						break;
+					case SDLK_s:
+					case SDLK_DOWN:
+						printf("DOWN pressed\n");
+						key_state[DOWN] = 1;
+						state_changed = 1;
+						break;
+					case SDLK_a:
+					case SDLK_LEFT:
+						printf("LEFT pressed\n");
+						key_state[LEFT] = 1;
+						state_changed = 1;
+						break;
+					case SDLK_d:
+					case SDLK_RIGHT:
+						printf("RIGHT pressed\n");
+						key_state[RIGHT] = 1;
+						state_changed = 1;
+						break;
+					case SDLK_q:
+					case SDLK_ESCAPE:
+						quit(0);
+						break;
+				}
+			}
+			else if(e.type == SDL_KEYUP)
+			{
+				switch(e.key.keysym.sym)
+				{
+					case SDLK_w:
+					case SDLK_UP:
+						printf("UP released\n");
+						key_state[UP] = 0;
+						state_changed = 1;
+						break;
+					case SDLK_s:
+					case SDLK_DOWN:
+						printf("DOWN released\n");
+						key_state[DOWN] = 0;
+						state_changed = 1;
+						break;
+					case SDLK_a:
+					case SDLK_LEFT:
+						printf("LEFT released\n");
+						key_state[LEFT] = 0;
+						state_changed = 1;
+						break;
+					case SDLK_d:
+					case SDLK_RIGHT:
+						printf("RIGHT released\n");
+						key_state[RIGHT] = 0;
+						state_changed = 1;
+						break;
+				}
+
+			}
 		}
+
+		if(state_changed)
+		{
+			//TODO: Change state!
+			state_changed = 0;
+		}
+		SDL_Delay(10);
 	}
 
 	return 0;
